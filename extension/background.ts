e0e384ef822aa237d5ad680085c2c033e49fb36a
n4ze3m <n4ze3m@gmail.com>
2023-08-08 00:48:32 +0530

    added few more changes

---
+++ extension/background.ts
@@ -0,0 +1,30 @@
+export {};
+
+chrome.omnibox.setDefaultSuggestion({
+  description: "Search WayToWebsite for %s",
+});
+
+chrome.omnibox.onInputEntered.addListener((text) => {
+//   const url = `https://www.com/search?q=${text}`;
+//   chrome.tabs.create({ url });
+
+});
+
+
+// show suggestions
+
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+
+    if (!text) {
+        return;
+    }
+
+    // we need do a api call here
+
+    suggest([
+        {
+            content: "",
+            description:"",
+        }
+    ]);
+});
